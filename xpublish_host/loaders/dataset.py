0c20a08edeb93aabad8f8fbc9316cd71dd2dcf76
Kristen Thyng <kthyng@gmail.com>
2023-08-09 13:27:06 -0700

    updated plus lint

--- xpublish_host/loaders/dataset.py
+++ xpublish_host/loaders/dataset.py
@@ -1,13 +1,20 @@
+import logging
+from pathlib import Path
+
 import xarray as xr
 
+L = logging.getLogger(__name__)
 
-def zarr(json_path: str | Path,):
+
+def load_dataset_zarr(json_path: str | Path,):
+
     ds = xr.open_dataset(
-    "reference://", engine="zarr",
-    backend_kwargs={
-        "storage_options": {
-            "fo": json_path,
-        },
-        "consolidated": False
-    }
-)
+        "reference://", engine="zarr",
+        backend_kwargs={
+            "storage_options": {
+                "fo": json_path,
+            },
+            "consolidated": False
+        }
+    )
+    return ds
