ba2fe4d86751ded848d7771c863cb661298bc40f
Kristen Thyng <kthyng@gmail.com>
2023-08-18 09:56:10 -0700

    print statements this time

--- xpublish_host/loaders/dataset.py
+++ xpublish_host/loaders/dataset.py
@@ -12,6 +12,10 @@ def load_dataset_zarr(json_path: str | Path, chunks=None):
     # encoding and dask arrays
     # https://github.com/xpublish-community/xpublish/issues/207
     chunks = chunks or {}
+    
+    print(f"Chunks are {chunks}.")
+    
+    print(f"using json from {json_path}")
 
     ds = xr.open_dataset(
         "reference://", engine="zarr",
@@ -26,6 +30,7 @@ def load_dataset_zarr(json_path: str | Path, chunks=None):
 
     # remove chunks encoding, same issue as above
     # https://github.com/xpublish-community/xpublish/issues/207
+    print("removing chunks from all variable encoding")
     for varname in ds.data_vars:
         del ds[varname].encoding["chunks"]
 
